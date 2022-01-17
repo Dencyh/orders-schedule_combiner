bd9d4a78e070da98e14de02e18f3554a1d588ad3
Alexander Moryan <a.dencyh@gmail.com>
2022-01-17 08:28:27 +0300

    add dropped ws

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
-import XLSX from 'xlsx'
-import fs from 'fs'
-import chalk from 'chalk'
+const XLSX = require('xlsx')
+const fs = require('fs')
+
 
 // Get file names
 const
@@ -90,22 +90,33 @@ couriersDropshipsReturn.forEach((courier) => {
 })
 
 
+const dropped = []
+
 // Sort by courier (first element) alphabetically
 const couriersSortedByName = couriersDropships.sort((a, b) => {
     if (a[0] < b[0]) {
         return -1
     }
 }).filter((element) => { //gets rid of the couriers that are not in the schedule and have 0 orders
-    if (element.length < 2) console.log(chalk.green('Нет заказов по прямому потоку : ' + element))
+    if (element.length < 2) {
+        dropped.push([element])
+        console.log('Нет заказов по прямому потоку : ' + element)
+    }
     return element.length > 1
 })
+
+
+const droppedReturn = []
 
 const couriersReturnSortedByName = couriersDropshipsReturn.sort((a, b) => {
     if (a[0] < b[0]) {
         return -1
     }
 }).filter((element) => { //gets rid of the couriers that are not in the schedule and have 0 orders
-    if (element.length < 2) console.log(chalk.green('Нет заказов по возвратному потоку: ' + element))
+    if (element.length < 2) {
+        droppedReturn.push([element])
+        console.log('Нет заказов по возвратному потоку: ' + element)
+    }
     return element.length > 1
 })
 
@@ -121,12 +132,12 @@ schedulesData.forEach((sheduleInfoRow) => {
     couriersFromSchedule.push(sheduleInfoRow['Курьер'])
 })
 
-couriersFromSchedule.forEach((courier) => {
-    if (couriersFromShifts.indexOf(courier) < 0) console.log(chalk.red('Не получил маршрут (заказы переназначены) : ' + courier))
-})
+/* couriersFromSchedule.forEach((courier) => {
+    if (couriersFromShifts.indexOf(courier) < 0) console.log('Не получил маршрут (заказы переназначены) : ' + courier)
+}) */
 
 couriersFromShifts.forEach((courier) => {
-    if (couriersFromSchedule.indexOf(courier) < 0) console.log(chalk.magenta('Убрали из расписания: ' + courier))
+    if (couriersFromSchedule.indexOf(courier) < 0) console.log('Убрали из расписания: ' + courier)
 })
 
 
@@ -174,15 +185,27 @@ couriersReturnSortedByCompany.unshift(['', 'ФИО', '', '', 'Адрес 1', '',
 
 
 // Creating new file and writing to it
-const dropshipsWS_name = 'Прямой поток'
-const dropshipsWS = XLSX.utils.aoa_to_sheet(couriersSortedByCompany)
-const dropshipsReturnWS_name = 'Возвратный поток'
-const dropshipsReturnWS = XLSX.utils.aoa_to_sheet(couriersReturnSortedByCompany)
 const newWB = XLSX.utils.book_new()
 
+const dropshipsWS_name = 'Прямой поток'
+const dropshipsWS = XLSX.utils.aoa_to_sheet(couriersSortedByCompany)
 XLSX.utils.book_append_sheet(newWB, dropshipsWS, dropshipsWS_name)
+
+const dropshipsReturnWS_name = 'Возвратный поток'
+const dropshipsReturnWS = XLSX.utils.aoa_to_sheet(couriersReturnSortedByCompany)
 XLSX.utils.book_append_sheet(newWB, dropshipsReturnWS, dropshipsReturnWS_name)
 
+if (dropped.length > 0) {
+    const droppedWS_name = 'Дропнуло Прямой поток'
+    const droppedWS = XLSX.utils.aoa_to_sheet(dropped)
+    XLSX.utils.book_append_sheet(newWB, droppedWS, droppedWS_name)
+}
+
+if (droppedReturn > 0) {
+    const droppedReturnWS_name = 'Дропнуло Возвратный поток'
+    const droppedReturnWS = XLSX.utils.aoa_to_sheet(droppedReturn)
+    XLSX.utils.book_append_sheet(newWB, droppedReturnWS, droppedReturnWS_name)
+}
 
 XLSX.writeFile(newWB, './ResultForm.xlsx')
 
