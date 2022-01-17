0e664fb1674b26c604431cb8859fdb88c1b3002c
Alexander Moryan <a.dencyh@gmail.com>
2022-01-17 22:02:33 +0300

    refactoring

--- src/app.js
+++ src/app.js
@@ -2,127 +2,44 @@ const XLSX = require('xlsx')
 const fs = require('fs')
 
 
+const FOLDER_PATH = './Schedules_Orders'
+
+
+
 // Get file names
-const
-    folderPath = './Schedules_Orders',
-    filesList = []
-
-let schedules = new RegExp(/schedules*/)
-let orders = new RegExp(/orders*/)
-
-let
-    schedulesFileName,
-    ordersFileName
-
-
-fs.readdirSync(folderPath).forEach(file => {
-    filesList.push(file)
-});
-
-filesList.forEach((fileName) => {
-    if (schedules.test(fileName)) {
-        schedulesFileName = fileName
-    } else if (orders.test(fileName)) {
-        ordersFileName = fileName
-    }
-})
+const files = getFileNames(FOLDER_PATH)
 
 // Get orders data
-const wbOrders = XLSX.readFile(`${folderPath}/${ordersFileName}`, { cellDates: true })
-const wsOrders = wbOrders.Sheets['отчет']
-
-const ordersData = XLSX.utils.sheet_to_json(wsOrders)
-
-// Getting schedules data
-const
-    wbSchedules = XLSX.readFile(`${folderPath}/${schedulesFileName}`, { cellDates: true }),
-    wsSchedules = wbSchedules.Sheets['Sheet1'],
-    schedulesData = XLSX.utils.sheet_to_json(wsSchedules)
+const ordersData = getData(files.orders, 'отчет')
+const schedulesData = getData(files.schedule, 'Sheet1')
 
 
 // Getting unique couriers
-let couriersFromShifts = []
+let couriersFromShifts = getCouriesList(ordersData)
 
-
-ordersData.forEach((orderEntry) => {
-    couriersFromShifts.push(orderEntry['Курьер'])
-})
-couriersFromShifts = Array.from(new Set([...couriersFromShifts])) // Unique couriers from shifts
 
 
 const couriersDropships = []
 const couriersDropshipsReturn = []
 
-schedulesData.forEach((courier) => {
-    if (courier['Тип транспортного средства'] == 'Дзерж Дропофф(только забор)') {
-        couriersDropships.push([courier['Курьер']])
-    } else if (courier['Тип транспортного средства'] == 'Возврат Дзерж КГТ') {
-        couriersDropshipsReturn.push([courier['Курьер']])
-    }
-})
+/* devide delivery and return */
+schedulesData.forEach((courier) => devideByType(courier))
 
+/* match address by name */
+/* delivery */
+couriersDropships.forEach(courier => matchAddress(courier))
+/* return */
+couriersDropshipsReturn.forEach((courier) => matchAddress(courier))
 
-
-couriersDropships.forEach((courier) => {
-    ordersData.forEach((orderInfo) => {
-        if (
-            courier[0] == orderInfo['Курьер']
-            && (orderInfo['Статус'] == 'В процессе'
-                || orderInfo['Статус'] == 'Выполнено')
-        ) {
-            courier.push(orderInfo['Адрес'])
-            courier.push('')
-        }
-    })
-})
-
-couriersDropshipsReturn.forEach((courier) => {
-    ordersData.forEach((orderInfo) => {
-        if (
-            courier[0] == orderInfo['Курьер']
-            && (orderInfo['Статус'] == 'В процессе'
-                || orderInfo['Статус'] == 'Выполнено')
-        ) {
-            courier.push(orderInfo['Адрес'])
-            courier.push('')
-        }
-    })
-})
 
 
 const dropped = []
-
-// Sort by courier (first element) alphabetically
-const couriersSortedByName = couriersDropships.sort((a, b) => {
-    if (a[0] < b[0]) {
-        return -1
-    }
-}).filter((element) => { //gets rid of the couriers that are not in the schedule and have 0 orders
-    if (element.length < 2) {
-        dropped.push([element])
-        console.log('Нет заказов по прямому потоку : ' + element)
-    }
-    return element.length > 1
-})
+/* delete dropped from the list or those  */
+const couriersSortedByName = findDropped(couriersDropships).sort(sortByChar)
 
 
 const droppedReturn = []
-
-const couriersReturnSortedByName = couriersDropshipsReturn.sort((a, b) => {
-    if (a[0] < b[0]) {
-        return -1
-    }
-}).filter((element) => { //gets rid of the couriers that are not in the schedule and have 0 orders
-    if (element.length < 2) {
-        droppedReturn.push([element])
-        console.log('Нет заказов по возвратному потоку: ' + element)
-    }
-    return element.length > 1
-})
-
-
-
-
+const couriersReturnSortedByName = findDropped(couriersDropshipsReturn).sort(sortByChar)
 
 
 
@@ -132,9 +49,6 @@ schedulesData.forEach((sheduleInfoRow) => {
     couriersFromSchedule.push(sheduleInfoRow['Курьер'])
 })
 
-/* couriersFromSchedule.forEach((courier) => {
-    if (couriersFromShifts.indexOf(courier) < 0) console.log('Не получил маршрут (заказы переназначены) : ' + courier)
-}) */
 
 couriersFromShifts.forEach((courier) => {
     if (couriersFromSchedule.indexOf(courier) < 0) console.log('Убрали из расписания: ' + courier)
@@ -142,62 +56,149 @@ couriersFromShifts.forEach((courier) => {
 
 
 
-// Append couriers' company name to the beggining of the row
-couriersSortedByName.forEach((courier) => {
-    schedulesData.forEach((schedule) => {
-        if (schedule['Курьер'] == courier[0]) {
-            const ordersCount = Math.round((courier.length - 1) / 2)
-            courier.splice(1, 0, schedule['Номер машины'], ordersCount)
-            courier.unshift(schedule['Компания'])
-        }
-    })
-})
+// Get info for active
+couriersSortedByName.forEach((courier) => insertFullInfo(courier))
+couriersReturnSortedByName.forEach((courier) => insertFullInfo(courier))
 
-couriersReturnSortedByName.forEach((courier) => {
-    schedulesData.forEach((schedule) => {
-        if (schedule['Курьер'] == courier[0]) {
-            const ordersCount = Math.round((courier.length - 1) / 2)
-            courier.splice(1, 0, schedule['Номер машины'], ordersCount)
-            courier.unshift(schedule['Компания'])
-        }
-    })
-})
-
-dropped.forEach(courier => {
-    schedulesData.forEach((schedule) => {
-        if (schedule['Курьер'] == courier[0]) {
-            courier.push(schedule['Компания'])
-        }
-    })
-})
-
-droppedReturn.forEach(courier => {
-    schedulesData.forEach((schedule) => {
-        if (schedule['Курьер'] == courier[0]) {
-            courier.push(schedule['Компания'])
-        }
-    })
-})
+// Get info for dropped
+dropped.forEach(courier => insertCompanyOnly(courier))
+droppedReturn.forEach(courier => insertCompanyOnly(courier))
 
 
-// Sort by company (!first element) alphabetically
-const couriersSortedByCompany = couriersSortedByName.sort((a, b) => {
-    if (a[0] < b[0]) {
-        return -1
-    }
-})
-
-const couriersReturnSortedByCompany = couriersReturnSortedByName.sort((a, b) => {
-    if (a[0] < b[0]) {
-        return -1
-    }
-})
+// Sort by company
+const couriersSortedByCompany = couriersSortedByName.sort(sortByChar)
+const couriersReturnSortedByCompany = couriersReturnSortedByName.sort(sortByChar)
 
 // Insert header to the result worksheet
 couriersSortedByCompany.unshift(['', 'ФИО', '', '', 'Адрес 1', '', 'Адрес 2', '', 'Адрес 3', '', 'Адрес 4', '', 'Адрес 5', '', 'Адрес 6', '', 'Адрес 7', '', 'Адрес 8', '', 'Адрес 9'])
-
-
 couriersReturnSortedByCompany.unshift(['', 'ФИО', '', '', 'Адрес 1', '', 'Адрес 2', '', 'Адрес 3', '', 'Адрес 4', '', 'Адрес 5', '', 'Адрес 6', '', 'Адрес 7', '', 'Адрес 8', '', 'Адрес 9'])
+
+
+/* class Sheet {
+    constructor(name, data, cols = [{}]) {
+        this.name = name
+        this.sheet = XLSX.utils.aoa_to_sheet(data)
+        this.sheet['!cols'] = cols // Can you do that?
+    }
+}
+
+function createFile(sheetsArr, fileName) {
+    const wb = XLSX.utils.book_new()
+
+
+    sheetsArr.forEach((sheet) => {
+        XLSX.utils.book_append_sheet(wb, sheet.data, sheet.name)
+    })
+
+    XLSX.utils.book_append_sheet(wb, ws, wsName)
+
+
+    XLSX.writeFile(wb, `./${fileName}.xlsx`)
+} */
+
+
+
+// Helper functions
+
+function getFileNames(folder) {
+    const files = {}
+    const filesList = []
+
+    let schedules = new RegExp(/schedules*/)
+    let orders = new RegExp(/orders*/)
+
+
+    fs.readdirSync(folder).forEach(file => {
+        filesList.push(file)
+    });
+
+    filesList.forEach((file) => {
+        if (schedules.test(file)) {
+            files.schedule = file
+        } else if (orders.test(file)) {
+            files.orders = file
+        }
+    })
+
+    return files
+}
+
+function getData(fileName, sheetName) {
+    const sheet = XLSX
+        .readFile(`${FOLDER_PATH}/${fileName}`, { cellDates: true })
+        .Sheets[sheetName]
+
+    const data = XLSX.utils.sheet_to_json(sheet)
+
+    return data
+}
+
+/* This is the only way not to miss anyone if they were removed from schedule by mistake */
+function getCouriesList(ordersData) {
+    let couriers = []
+    ordersData.forEach((orderEntry) => {
+        couriers.push(orderEntry['Курьер'])
+    })
+    couriers = Array.from(new Set([...couriers]))
+    return couriers
+}
+
+function matchAddress(courier) {
+    ordersData.forEach(orderInfo => {
+        if (
+            courier[0] == orderInfo['Курьер']
+            && (orderInfo['Статус'] == 'В процессе' || orderInfo['Статус'] == 'Выполнено')
+        ) {
+            courier.push(orderInfo['Адрес'])
+            courier.push('')
+        }
+    })
+}
+
+function devideByType(courier) {
+    if (courier['Тип транспортного средства'] == 'Дзерж Дропофф(только забор)') {
+        couriersDropships.push([courier['Курьер']])
+    } else if (courier['Тип транспортного средства'] == 'Возврат Дзерж КГТ') {
+        couriersDropshipsReturn.push([courier['Курьер']])
+    }
+}
+
+function findDropped(couriers) {
+    return (
+        couriers.filter((element) => {
+            if (element.length < 2) {
+                dropped.push([element])
+            }
+            return element.length > 1
+        })
+    )
+}
+
+function insertFullInfo(courier) {
+
+    schedulesData.forEach((schedule) => {
+        if (schedule['Курьер'] == courier[0]) {
+            const ordersCount = Math.round((courier.length - 1) / 2)
+            courier.splice(1, 0, schedule['Номер машины'], ordersCount)
+            courier.unshift(schedule['Компания'])
+        }
+    })
+
+}
+
+function insertCompanyOnly(courier) {
+    schedulesData.forEach((schedule) => {
+        if (schedule['Курьер'] == courier[0]) {
+            courier.push(schedule['Компания'])
+        }
+    })
+}
+
+function sortByChar(a, b) {
+    if (a[0] < b[0]) {
+        return -1
+    }
+}
 
 
 // Creating new file and writing to it
